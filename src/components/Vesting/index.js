2c83f648722a5e3f80473df02fe9b0e3d6584884
PS Crypt <psocrypt@gmail.com>
2020-12-19 18:23:22 +0530

    css fix

--- src/components/Vesting/index.js
+++ src/components/Vesting/index.js
@@ -522,7 +522,7 @@ const Vesting = () => {
                                         </div>
                                         :
                                         <div
-                                            style={{ marginTop: "20%", paddingBottom: "18%" }}
+                                            style={{ marginTop: "17%", paddingBottom: "17.2%" }}
                                         >
                                             <p style={{ textAlign: "center", fontSize: "large", color: "orange" }}>
                                                 You have not connected to metamask or You don't have Metamask.<br />
